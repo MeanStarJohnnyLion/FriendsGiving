a1817f50a71bf0eadad343482e6a7bce37d5c7df
Johnny Morgan <johnnylion3@aol.com>
2022-11-21 23:10:06 -0500

    Initial Commit

---
+++ FriendsGiving/FriendsGivingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsGivingApp.swift
+//  FriendsGiving
+//
+//  Created by Johnny Lion on 11/21/22.
+//
+
+import SwiftUI
+
+@main
+struct FriendsGivingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
